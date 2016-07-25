b90de85922461b86f745cf5097c9542ca8057250
José Antonio Casares González <josecasares@gmail.com>
2016-07-25 23:59:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='UWS',
-      version='1.4',
+      version='1.5',
       description='Universal Web SCADA',
       author='José Antonio Casares González',
       author_email='josecasares@gmail.com',
